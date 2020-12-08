739c5976766638a69e70f9fcf174774c79324683
Hieuo0 <vuhieukenny2@gmail.com>
2020-12-08 17:33:19 +0700

    Add cat-eat-animal method

--- cat.js
+++ cat.js
@@ -1,10 +1,17 @@
 const Mouse = require('./Mouse'); 
+
 function Cat (name){
 	this.name = name,
 	this.stomach = []
 }
-Cat.prototype.eat = function(Mouse){
-	Mouse.eat(); 
-	this.stomach.push(Mouse); 
+Cat.prototype.eat = function(animal){
+	if(animal instanceof Mouse){
+	animal.eat(); 
+	this.stomach.push(animal);
+	}
+	 else {
+	 	throw new Error('Have some error in your code'); 
+	 }
 }
+
 module.exports = Cat; 
